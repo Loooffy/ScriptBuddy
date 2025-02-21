611410dc0eb9afb8bdd670f24b323c5421bf36ff
Loooffy <Loooffy>
2025-02-21 09:14:01 +0800

    Use basename for static assets like image.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/ScriptBuddy'>
       <div className="app">
         <header className="app-header">
           <SrtPlayer />
